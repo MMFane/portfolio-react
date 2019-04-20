3189e040ac581bc467a593a2ef08aad21afdfb5b
Sam Yeager <sam.williams@littlebytegames.com>
2019-04-20 09:25:23 -0700

    implement hero styling to make headers

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -2,16 +2,45 @@ import * as React from 'react';
 import Banner from './banner/Banner';
 import Hero from './hero/Hero';
 import Nav from './nav/Nav';
-import { hot } from 'react-hot-loader/root'
+import ResponsiveImage from './responsive-image/ResponsiveImage';
+import logo from '../../images/signature.png';
+// import background from '../../images/bg-chalkboard.jpg';
+import { hot } from 'react-hot-loader/root';
+import FlexGroup from './flex-group/FlexGroup';
+import ImageButton from './image-button/ImageButton';
 
 class App extends React.Component<any, any> {
-
   render() {
     return (
       <>
       <Nav />
+      <Hero classes="first" color="black" height="large"
+      // image={background}
+      >
+        <div className="content">
+          <ResponsiveImage classes="logo" src={logo} width={100}/>
+          <h1>Samantha Yeager</h1>
+          <h2><em>UI / UX Designer</em></h2>
+        </div>
+      </Hero>
+      <Hero classes="second" color="black"
+      // image={background}
+      >
+        <div className="content">
+        <FlexGroup classes="justify-center align-i-center mt-1">
+          <ImageButton classes="mr-3 first" src="" text="Project 1"/>
+          <ImageButton classes="mr-3" src="" text="Project 2"/>
+          <ImageButton classes="last" src="" text="Project 3"/>
+        </FlexGroup>
+        </div>
+      </Hero>
       <Banner content="Portfolio" />
-      <Hero content="Hero Image" />
+      <Banner content="Portfolio" />
+      <Banner content="Portfolio" />
+      <Banner content="Portfolio" />
+      <Banner content="Portfolio" />
+      <Banner content="Portfolio" />
+      <Banner content="Portfolio" />
       </>
     );
   }
--- src/js/components/hero/Hero.tsx
+++ src/js/components/hero/Hero.tsx
@@ -1,14 +1,30 @@
 import * as React from 'react';
 import './hero.css';
 
+export interface HeroProps {
+  readonly color: string;
+  readonly image: string;
+  readonly height: string;
+  readonly classes: string;
+}
+
 class Hero extends React.Component<any, any> {
   render() {
+
+    const children = this.props.children;
+    const color = this.props.color;
+    const image = this.props.image;
+    const height = this.props.height;
+    const classes = this.props.classes;
+
     return (
         <>
-            <div className="hero">
-                <h1 className="hero-content">{this.props.content}</h1>
+            <div className={`hero ${classes ? classes : ''} ${height ? height : ''}`}>
+                <div className="hero-content">
+                    { children }
+                </div>
                 <div className="hero-bg"></div>
-                <div className="hero-color"></div>
+                <div className={`hero-color ${color}`}></div>
             </div>
         </>
     );
