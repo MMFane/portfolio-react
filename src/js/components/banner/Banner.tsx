1b0553dc32ed60011e5fbe07bf18fc647dadc3da
Sam Yeager <sam.williams@littlebytegames.com>
2019-04-21 16:08:01 -0700

    style 2-layer footer with bg colors and images

--- src/js/components/banner/Banner.tsx
+++ src/js/components/banner/Banner.tsx
@@ -3,15 +3,19 @@ import './banner.css';
 
 export interface BannerProps {
     readonly classes: string;
+    readonly bgColor: string;
+    readonly bgImage: string;
 }
 
 class Banner extends React.Component<BannerProps> {
   render() {
     const children = this.props.children;
     const classes = this.props.classes;
+    const bgColor = this.props.bgColor;
+    const bgImage = this.props.bgImage;
 
     return (
-      <div className={`banner ${classes ? classes : ''}`}>
+        <div className={`banner ${classes ? classes : ''} ${bgColor ? bgColor : ''} ${bgImage ? 'bg-'+bgImage : ''}`}>
             {children}
         </div>
     );
