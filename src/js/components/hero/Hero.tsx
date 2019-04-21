8cea6919c8ac04b401bd22b74275edc21e48bf56
Sam Yeager <sam.williams@littlebytegames.com>
2019-04-21 07:48:01 -0700

    add hero angles and casino background

--- src/js/components/hero/Hero.tsx
+++ src/js/components/hero/Hero.tsx
@@ -3,6 +3,7 @@ import './hero.css';
 
 export interface HeroProps {
   readonly classes: string;
+  readonly angle: string;
   readonly color: string;
   readonly height: string;
   readonly image: string;
@@ -13,13 +14,14 @@ class Hero extends React.Component<any, any> {
 
     const children = this.props.children;
     const classes = this.props.classes;
+    const angle = this.props.angle;
     const color = this.props.color;
     const height = this.props.height;
     const image = this.props.image;
 
     return (
         <>
-            <div className={`hero ${classes ? classes : ''} ${height ? height : ''}`}>
+            <div className={`hero ${classes ? classes : ''} ${angle ? "angle-"+angle : ''} ${height ? height : ''}`}>
                 <div className="hero-content">
                     { children }
                 </div>
