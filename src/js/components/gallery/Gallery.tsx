4cc6ddd45109cefdbeff3b20f0338a96b695c371
Sam Yeager <sam.williams@littlebytegames.com>
2019-04-29 11:45:01 -0700

    implement masonry in Gallery component

--- src/js/components/gallery/Gallery.tsx
+++ src/js/components/gallery/Gallery.tsx
@@ -1,19 +1,42 @@
 import * as React from 'react';
+import Masonry from 'react-masonry-component';
 import './gallery.css';
+
+const masonryOptions = {
+    transitionDuration: 0
+};
+
 
 export interface GalleryProps {
     readonly classes: string;
+    readonly elements: Array<string>;
 }
 
 class Gallery extends React.Component<GalleryProps> {
     render() {
         const classes = this.props.classes;
-        const children = this.props.children;
+        const elements = this.props.elements;
+        // const children = this.props.children;
+
+        const childElements = elements.map(function (element) {
+            return (
+                <li className="image-element-class" key={element}>
+                    <img src={element} />
+                </li>
+            );
+        });
 
         return (
-            <div className={`gallery ${classes}`}>
-                {children}
-            </div>
+            <Masonry
+                className={`gallery ${classes ? classes : ''}`}
+                elementType={'ul'} // default 'div'
+                options={masonryOptions}
+                disableImagesLoaded={false}
+                updateOnEachImageLoad={false}
+                // imagesLoadedOptions={imagesLoadedOptions}
+            >
+                {childElements}
+            </Masonry>
         );
     }
 }
